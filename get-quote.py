9ddd2b16eef0c19f672ca4cd21ab48b206d3a2c0
Michael Baumgarten <michaelbaumgarten@Michaels-Air-4.fritz.box>
2020-12-12 20:24:59 +0100

    incl. extra tasks

--- get-quote.py
+++ get-quote.py
@@ -1,15 +1,28 @@
 import random
 def primary():
   print("Keep it logically awesome.")
-
+  
+  f = open("quotes.txt", "a")
+  f.write("Bananen Wurst und Tee \n")
+  f.close()
+  
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
   
-  last = 13
-  rnd = random.randint(0, last)
+  newQuote = input("Enter a quote:")
   
-  print(quotes[rnd])
+  quotes.append("Wer anderen eine Bratwurst brät.")
+  quotes.append(newQuote)
+
+  last = len(quotes)
+  rnd = random.randint(0, last)
+  rnd2 = random.randint(0, last)
+  
+  for x in quotes:
+    # quotes = quotes.rstrip("\n")
+    print(x)
+  print(quotes[rnd], quotes[rnd2], quotes[last - 1 ])
 
 if __name__== "__main__":
   primary()
